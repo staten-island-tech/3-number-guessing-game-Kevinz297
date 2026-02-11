963bf49df80f90270399a0ecc353f9b50354c1c0
Kevinz297 <Kevinz297@nycstudents.net>
2026-02-11 09:23:47 -0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,9 @@
+count = 1
+
+while count <= 5:
+    print("Yes, the number is less than 5")
+    if count < 5:
+        count += 1
+    elif count >= 5:
+        print("No, the number is not less than 5")
+        break
