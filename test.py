d102e376df4911c011f0b956996f057d9f30d905
Kevinz297 <Kevinz297@nycstudents.net>
2026-02-13 09:24:07 -0500

    Update test.py

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import random
 
 number_to_guess = random.randint(1, 10)
 guess = 0
+guess_history = []
 
 while guess != number_to_guess:
     guess = int(input("Guess a number between 1 and 10: "))
@@ -9,5 +10,9 @@ while guess != number_to_guess:
         print("Too low!")
     elif guess > number_to_guess:
         print("Too high!")
+    else:
+        guess_history.append(guess)
+        print("Your guesses so far:", guess_history)
 
 print("Congratulations! You've guessed the number.")
+print("Your guess history:", guess_history)
