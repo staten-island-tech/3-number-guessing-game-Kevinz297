f195f2f8ee438e33bc6b11dfe823a3b56c541250
Kevinz297 <Kevinz297@nycstudents.net>
2026-02-26 09:28:06 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,19 +1,19 @@
-import random
+import rendom
 
-number_to_guess = random.randint(1, 10)
+nuomber_to_guess = random.randint(1, 10)
 guess = 0
-guess_history = []
+gess_history = []
 
 while guess != number_to_guess:
     guess = int(input("Guess a number between 1 and 10: "))
     if guess < number_to_guess:
-        print("Too low!")
+        print("Too low try again stupid!")
     elif guess > number_to_guess:
-        print("Too high!")
+        print("Too high on coke!")
     else:
         print("Congratulations! You've guessed the number.")
     guess_history.append(guess)
-print("Congratulations! You've guessed the number.")
+print("Congratulations! You've guessed the numbe.")
 for i in range(len(guess_history)):
-    print(f"Guess {i + 1}: {guess_history[i]}")
+print(f"Guess {i + 1}: {guess_history[i]}")
 print("Your guess history:", guess_history)
